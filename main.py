dab2d3f6ceb072f63952ce4cd5977bc897bae0e3
Capptn.com Fabian Gdanietz <info@capptn.com>
2024-08-06 05:49:11 +0000

    added firestore

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask, json, request
 from solarindex3 import calculate_actual_solar_indices, calculate_daily_solar_indices
+import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("./ai-pv-forecast-firebase-adminsdk-fu9qk-3fbb251864.json")
+app = firebase_admin.initialize_app(cred)
+store = firestore.client()
+
+def get_settings_from_firestore(uid):
+
+  doc_ref = store.collection("pv_settings").document(uid)
+
+  doc = doc_ref.get()
+  if doc.exists:
+      return doc.to_dict()
+  else:
+      return False
 
 
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
 api = Flask(__name__)
 
@@ -16,15 +32,19 @@ def get_companies():
 @api.route('/daily', methods=['GET'])
 def get_dialy():
   date = request.args.get('date')
-  lat = float(request.args.get('lat'))
-  lon = float(request.args.get('lon'))
-  tilt = int(request.args.get('tilt'))
-  ori = int(request.args.get('ori'))
+  data = get_settings_from_firestore(request.args.get('uid'))
+  if data == False:
+     return 'bad request!', 400
+  print(data)
+  lat = float(data['lat'])
+  lng = float(data['lng'])
+  tilt = int(data['roofPitch'])
+  ori = int(data['orientation'])
   print(date)
-  print(lon)
+  print(lng)
   print(lat)
   value = calculate_daily_solar_indices(
-      date, lat, lon, ori, tilt
+      date, lat, lng, ori, tilt
   )
   return json.dumps(value)
 
